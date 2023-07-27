cb1d08f6d1b0d00f7ba48b5da851b8d4ce7fdce9
FranklinShack <franklinshack@gmail.com>
2023-07-27 18:55:16 -0400

    problem 1

---
+++ rust/1-9/problem1/target/debug/deps/problem1-8586311022917899.d
@@ -0,0 +1,5 @@
+/home/franklin/Documents/project-euler/rust/1-9/problem1/target/debug/deps/problem1-8586311022917899: src/main.rs
+
+/home/franklin/Documents/project-euler/rust/1-9/problem1/target/debug/deps/problem1-8586311022917899.d: src/main.rs
+
+src/main.rs:
---
+++ rust/1-9/problem1/target/debug/problem1.d
@@ -0,0 +1,1 @@
+/home/franklin/Documents/project-euler/rust/1-9/problem1/target/debug/problem1: /home/franklin/Documents/project-euler/rust/1-9/problem1/src/main.rs
